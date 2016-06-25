6885a8a232a2f54735389fd88d8c8e88ecc497ba
Vikas <vikascjadhav@gmail.com>
2016-06-25 23:19:21 +0530

    gulp tutorial working

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+// Include gulp
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var notify = require('gulp-notify');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+var changed = require('gulp-changed');
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+
+
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+  gulp.src(['./src/css/*.css'])
+    .pipe(rename({suffix: '-all-min'}))	
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./dist/css/'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename({suffix:'-all-min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+// minify new or changed HTML pages
+gulp.task('htmlminify', function() {
+  var htmlSrc = './src/*.html',
+      htmlDst = './dist';
+
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/css/*.css',['styles']);
+    gulp.watch('src/*.html',['htmlminify']);
+});
+
+// Default Task
+//gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+
+gulp.task('default', ['styles','lint', 'scripts','htmlminify','watch']);
+
